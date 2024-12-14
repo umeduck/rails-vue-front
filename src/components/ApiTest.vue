d7f2cd3813e2b1095fdc04faf68de8c9b3341cbd
umemura <ume.daihuku0116.0707@gmail.com>
2024-12-14 11:07:51 +0900

    api test

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>ApiTest</h1>
+    <button @click="test">ApiTest</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    test() {
+      fetch('http://localhost:3000/api/test')
+        .then((response) => response.json())
+        .then((data) => {
+          this.message = data.message
+        })
+    }
+  }
+}
+</script>
